50b7541a5d1ab19ecf7a619d388f856eed438e6e
Norbert Nemetz <maxpowers961@gmail.com>
2019-10-27 15:39:19 +0100

    Initial commit

---
+++ app/src/main/java/com/nemetz/ble2cloud/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nemetz.ble2cloud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
