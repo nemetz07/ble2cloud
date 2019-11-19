f8d8c057b16de00cdd6159ba7f331c2a215c7eb7
Norbert Nemetz <maxpowers961@gmail.com>
2019-11-19 20:09:23 +0100

    Finished rename fix

--- app/src/main/java/com/nemetz/ble2cloud/connection/BLEScanFilter.kt
+++ app/src/main/java/com/nemetz/ble2cloud/connection/BLEScanFilter.kt
@@ -2,7 +2,7 @@ package com.nemetz.ble2cloud.connection
 
 import android.bluetooth.le.ScanFilter
 
-object MyScanFilter {
+object BLEScanFilter {
 
     val SCAN_FILTER_EMPTY = listOf<ScanFilter>(ScanFilter.Builder().build())
 
--- app/src/main/java/com/nemetz/ble2cloud/connection/BLEScanSettings.kt
+++ app/src/main/java/com/nemetz/ble2cloud/connection/BLEScanSettings.kt
@@ -2,7 +2,7 @@ package com.nemetz.ble2cloud.connection
 
 import android.bluetooth.le.ScanSettings
 
-object MyScanSettings {
+object BLEScanSettings {
 
     val SCAN_SETTINGS_AGGRESSIVE = ScanSettings.Builder()
         .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
